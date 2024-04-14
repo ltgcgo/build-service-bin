aaff39bfc5464cd37b517542de309a50150f9cf9
Lumière Élevé <88174309+PoneyClairDeLune@users.noreply.github.com>
2024-04-14 18:19:06 +0000

    Compress also.

--- sh/build.sh
+++ sh/build.sh
@@ -7,6 +7,15 @@ shx echob Setting Windows executables...
 ls -1 *_windows_* | while IFS= read -r file; do
 	mv "${file}" "${file}.exe"
 done
+rm index.htm
+shx echob Compressing files...
+ls -1 | while IFS= read -r file; do
+	if [[ "$file" == *".bz2" ]]; then
+		rm -f "$file"
+	else
+		bzip2 -9 "$file"
+	fi
+done
 shx echob Generating web page...
 bash ../lang/page.sh
 exit
